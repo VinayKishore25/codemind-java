deae46ea149e8ca9ef015e2b678f082fe32ca356
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2023-10-08 19:23:02 +0530

    import java.util.*; public class owl {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         while(t>0)         {             double x1 = sc.nextDouble();             double x2 = sc.nextDouble();             double y1 = sc.nextDouble();             double y2 = sc.nextDouble();             if(y1/x1 == y2/x2)             System.out.println("0");             else if(y1/x1 > y2/x2)             System.out.println("1");             else             System.out.println("-1");             t--;         }     } }

---
+++ Car_Choice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class owl
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            double x1 = sc.nextDouble();
+            double x2 = sc.nextDouble();
+            double y1 = sc.nextDouble();
+            double y2 = sc.nextDouble();
+            if(y1/x1 == y2/x2)
+            System.out.println("0");
+            else if(y1/x1 > y2/x2)
+            System.out.println("1");
+            else
+            System.out.println("-1");
+            t--;
+        }
+    }
+}
